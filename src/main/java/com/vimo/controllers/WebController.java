982ea83ac64bd29660b4a39b8f7cf2e0563ed530
Murshid Hassen <mhassen@zaizi.com>
2019-05-17 09:27:54 +0530

    WebController added

---
+++ src/main/java/com/vimo/controllers/WebController.java
@@ -0,0 +1,17 @@
+package com.vimo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class WebController{
+    @RequestMapping("/")
+    @ResponseBody
+    public String index(){
+        return "This is pretty basic";
+    }
+}
+
+
+
