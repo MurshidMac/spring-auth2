794b613d9f68f4350b751f746259cbb79c51230f
Murshid Hassen <mhassen@zaizi.com>
2019-05-17 09:48:48 +0530

    added configurations

---
+++ src/main/java/com/vimo/config/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.vimo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
+    }
+
+    // Hardcoded Example
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user")
+        .password("{noop}pass").roles("USER");
+    }
+
+}
+
