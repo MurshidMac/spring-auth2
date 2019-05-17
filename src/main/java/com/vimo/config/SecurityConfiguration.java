a5d1557b6783b8deff7f50000c75a6b390727560
Murshid Hassen <mhassen@zaizi.com>
2019-05-17 09:59:48 +0530

    configurations added

--- src/main/java/com/vimo/config/SecurityConfiguration.java
+++ src/main/java/com/vimo/config/SecurityConfiguration.java
@@ -3,14 +3,16 @@ package com.vimo.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     @Override
     public void configure(HttpSecurity http) throws Exception{
-        http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
+        http.authorizeRequests().antMatchers("/public/**").permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login.html").failureUrl("/login-error.html").permitAll();
     }
 
     // Hardcoded Example
--- src/main/java/com/vimo/controllers/WebController.java
+++ src/main/java/com/vimo/controllers/WebController.java
@@ -1,6 +1,7 @@
 package com.vimo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,6 +11,17 @@ public class WebController{
     @ResponseBody
     public String index(){
         return "This is pretty basic";
+    }
+    
+    @RequestMapping("/login.html")
+    public String login(){
+        return "login.html";
+    }
+
+    @RequestMapping("login-error.html")
+    public String loginError(Model model){
+        model.addAttribute("loginError", true);
+        return "login.html";
     }
 }
 
